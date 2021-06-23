74e34848f19e39be689c29928cf35d3fb1c102ef
Immanuel Sohn <info@immanuelsohn.de>
2021-06-23 15:03:20 +0200

    Setup Functions

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
-#!bin/sh
+#!/bin/bash
+BOLD="\e[1m"
+RED="\e[31m"
+BLUE="\e[34m"
 CYAN="\e[36m"
-echo -e "$CYANWelcome to my Script."
+LIGHT_RED="\e[91m"
+
+clear
+echo -e "$BLUE Welcome to my Script."
+if [ "$EUID" -ne 0 ]
+    then 
+        echo -e "$BOLD$RED PLEASE RUN AS ROOT!"
+    exit
+else echo -e "$LIGHT_RED DU BIST COOL UND HAST RECHTE"
+fi
+
